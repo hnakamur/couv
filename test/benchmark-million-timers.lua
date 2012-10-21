2d77cca556fb1092c8a0f725de1aed5ceddf7625
Hiroaki Nakamura <hnakamur@gmail.com>
2012-10-22 07:23:01 +0900

    Add million timers benchmark.

---
+++ test/benchmark-million-timers.lua
@@ -0,0 +1,24 @@
+local uv = require 'couv'
+
+local NUM_TIMERS = 1000 * 1000
+local timerCbCalled = 0
+
+function timerCb(handle)
+  timerCbCalled = timerCbCalled + 1
+end
+
+local timers = {}
+local timeout = 0
+for i = 1, NUM_TIMERS do
+  if i % 1000 == 0 then
+    timeout = timeout + 1
+  end
+  timers[i] = uv.timer_create()
+  uv.timer_start(timers[i], timerCb, timeout, 0)
+end
+
+local before = uv.hrtime()
+uv.run()
+local after = uv.hrtime()
+assert(timerCbCalled == NUM_TIMERS)
+print(string.format("million-timers: %.2f seconds", (after - before) / 1e9))
